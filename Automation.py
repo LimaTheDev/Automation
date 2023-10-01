50a50a2e42ced91eb863b61e90807c07c5c50d7f
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-10-01 13:34:28 -0300

    Update Automation.py
    Finalizado os ajustes e o adicionado um loop para cadastro dos produtos

--- Automation.py
+++ Automation.py
@@ -7,7 +7,7 @@ import pandas as pd
 #pyautogui,hotkey
 
 
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 0.3
 
 pyautogui.press("win")
 pyautogui.write("Chrome")
@@ -28,38 +28,51 @@ pyautogui.write(senha)
 
 pyautogui.click(x=984, y=551)
 pyautogui.press("enter")
-time.sleep(3)
+time.sleep(2)
 
 
 tabela = pd.read_csv("F:\intensivos\HashtagPython\Aula 1\Automation\produtos.csv")
 
 print(tabela)
 
-time.sleep(2)
+for linha in tabela.index:
 
-pyautogui.click(x=739, y=286)
+    pyautogui.click(x=739, y=286)
+    
+    codigo = tabela.loc[linha, "codigo"]
+    marca = tabela.loc[linha, "marca"]
+    tipo = tabela.loc[linha, "tipo"]
+    categoria = tabela.loc[linha, "categoria"]
+    preco = tabela.loc[linha, "preco_unitario"]
+    custo = tabela.loc[linha, "custo"]
+        
+    
+     
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
 
-pyautogui.write("Codigo")
-pyautogui.press("tab")
+    pyautogui.write(str(marca))
+    pyautogui.press("tab")
 
-pyautogui.write("Marca")
-pyautogui.press("tab")
+    pyautogui.write(str(tipo))
+    pyautogui.press("tab")
 
-pyautogui.write("Tipo")
-pyautogui.press("tab")
+    pyautogui.write(str(categoria))
+    pyautogui.press("tab")
 
-pyautogui.write("Categoria")
-pyautogui.press("tab")
+    pyautogui.write(str(preco))
+    pyautogui.press("tab")
 
-pyautogui.write("Preco")
-pyautogui.press("tab")
+    pyautogui.write(str(custo))
+    pyautogui.press("tab")
+    
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(obs))
+    pyautogui.press("tab")
 
-pyautogui.write("Custo")
-pyautogui.press("tab")
+    pyautogui.press("enter")
 
-pyautogui.write("Obs")
-pyautogui.press("tab")
-
-pyautogui.press("enter")
-
-pyautogui.scroll(500)
+    pyautogui.scroll(1000)
+    
+enviar_email()
