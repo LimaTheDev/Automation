d2637b5ab75809ba7c8bc80d1e4e2c16ee9b4c58
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-10-01 13:07:42 -0300

    Update Automation.py
    Atualizando o Time Sleep e adicionando itens a tabela

--- Automation.py
+++ Automation.py
@@ -1,10 +1,11 @@
 import pyautogui
 import time
-
+import pandas as pd
 #pyautogui.click
 #pyautogui.write
 #pyautogui.press
 #pyautogui,hotkey
+
 
 pyautogui.PAUSE = 0.5
 
@@ -15,7 +16,7 @@ pyautogui.press("enter")
 link = ("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(3)
+time.sleep(2)
 
 pyautogui.click(x=850, y=400)
 email = ("Junior371098@gmail.com")
@@ -28,3 +29,37 @@ pyautogui.write(senha)
 pyautogui.click(x=984, y=551)
 pyautogui.press("enter")
 time.sleep(3)
+
+
+tabela = pd.read_csv("F:\intensivos\HashtagPython\Aula 1\Automation\produtos.csv")
+
+print(tabela)
+
+time.sleep(2)
+
+pyautogui.click(x=739, y=286)
+
+pyautogui.write("Codigo")
+pyautogui.press("tab")
+
+pyautogui.write("Marca")
+pyautogui.press("tab")
+
+pyautogui.write("Tipo")
+pyautogui.press("tab")
+
+pyautogui.write("Categoria")
+pyautogui.press("tab")
+
+pyautogui.write("Preco")
+pyautogui.press("tab")
+
+pyautogui.write("Custo")
+pyautogui.press("tab")
+
+pyautogui.write("Obs")
+pyautogui.press("tab")
+
+pyautogui.press("enter")
+
+pyautogui.scroll(500)
